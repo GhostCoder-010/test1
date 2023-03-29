b3d9ff79a2d37909c5f840dbc54235be1ef4786f
GhostCoder-010 <irfanashrafnechikkad@gmail.com>
2023-03-29 15:58:39 +0530

    CompanyRegister API created

--- accounts/views.py
+++ accounts/views.py
@@ -32,5 +32,62 @@ class CompanyCheck(APIView):
                 'errors': 'Company doesn\'t exist'
             }
         )
+        
+        
+class CompanyRegister(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny, ]
+
+    def post(self, request):
+        company_name = request.data.get('company_name')
+        username = request.data.get('username')
+        password = request.data.get('password')
+        mobile = request.data.get('mobile')
+        email = request.data.get('email')
+        logo = request.FILES.get('logo')
+
+        if check_company_exist():
+
+            return Response(
+                {
+                    'success': False,
+                    'data': None,
+                    'message': 'Company Details already exist, Please login with your company credentials',
+                    'errors': 'Company already exist'
+                }
+            )
+
+        user = User.objects.create_user(
+            name=company_name,
+            username=username,
+            password=password,
+            mobile=mobile,
+            email=email,
+            fk_role='company',
+        )
+        user_serializer = UserSerializer(user)
+
+        company = CompanyProfile.objects.create(
+            company_name=company_name,
+            email=email,
+            mobile=mobile,
+            fkAdmin=user,
+            logo = logo
+        )
+        company_serializer = CompanyProfileSerializer(company)
+
+        data = {
+            'company': company_serializer.data,
+            'user': user_serializer.data
+        }
+
+        return Response(
+            {
+                'success': True,
+                'data': data,
+                'message': 'Company Details created successfully ',
+                'errors': None
+            }
+        )
 
 # Create your views here.
